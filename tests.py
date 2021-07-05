ebca5d36752ec7da9ff58f1602732d14508b676a
Fran Asensi <asensi.benito@gmail.com>
2021-07-05 14:59:09 +0200

    Test create banck entry

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from pony.orm import db_session
+from pony.converting import str2datetime
 import entities
 
 
@@ -9,3 +10,24 @@ def test_create_category():
     cat1 = entities.Category(name=test_name)
 
     assert cat1.name == test_name
+
+
+@db_session
+def test_create_subcategory():
+    test_name = 'TEST'
+
+    cat1 = entities.Subcategory(name=test_name)
+
+    assert cat1.name == test_name
+
+
+@db_session
+def test_create_bank_entry():
+    bank_entry = entities.BankEntry(
+        entryDate=str2datetime('2012-10-20 15:22:00'),
+        ingCategory=1,
+        ingSubcategory=1,
+        amount=100
+        )
+
+    assert bank_entry.ingCategory != 1
