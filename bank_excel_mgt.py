fde8f246758854cf086afeaed8e1ad7d666b8b83
Fran Asensi <asensi.benito@gmail.com>
2021-07-07 12:44:30 +0200

    Refactor environ variables

--- bank_excel_mgt.py
+++ bank_excel_mgt.py
@@ -1,22 +1,23 @@
+import myconstants
 import os
 from os import walk
-from dotenv import load_dotenv
 import xlrd
-
-load_dotenv()
 
 
 def run_import_process():
-    process_path = os.getenv('PROCESS_PATH')
-    processed_path = os.getenv('PROCESSED_PATH')
+    process_input_files(myconstants.PROCESS_PATH)
 
-    process_input_files(process_path)
+    # TODO: Move processed files
 
 
 def process_input_files(path):
     os.chdir(path)
 
-    processed_list = [import_file(file) for (dirpath, dirnames, filenames) in walk(path) for file in filenames]
+    processed_list = [
+        import_file(file)
+        for (dirpath, dirnames, filenames) in walk(path)
+        for file in filenames
+    ]
 
     print(processed_list)
 
@@ -25,12 +26,15 @@ def import_file(file):
     wb = xlrd.open_workbook(file)
 
     sheet_obj = wb.sheet_by_index(0)
-    max_col = sheet_obj.ncols
     max_row = sheet_obj.nrows
+    initial_row = int(myconstants.INITIAL_ROW)
 
-    for cur_row in range(0, max_row):
-        for cur_col in range(0, max_col):
-            cell = sheet_obj.cell(cur_row, cur_col)
-            print(cell.value)
+    # Extrat data for each row and insert into database
+    [get_row_data(sheet_obj.row(cur_row)) for cur_row in range(initial_row, max_row)]
 
     return file
+
+
+def get_row_data(row):
+    # TODO: Extrat fields and insert into database
+    pass
