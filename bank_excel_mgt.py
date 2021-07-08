6a1ff8ced017808dae955ba929c85d0cfde8fee0
Fran Asensi <asensi.benito@gmail.com>
2021-07-08 08:35:00 +0200

    Refactor import excel code

--- bank_excel_mgt.py
+++ bank_excel_mgt.py
@@ -5,21 +5,27 @@ import xlrd
 
 
 def run_import_process():
-    process_input_files(myconstants.PROCESS_PATH)
+    # Get files to process
+    files_to_process = get_input_files(myconstants.PROCESS_PATH)
 
-    # TODO: Move processed files
+    # Get all rows of files to process
+    file_rows = [import_file(file) for file in files_to_process]
+
+    # Store in database
+    [insert_row_data(row) for rows in file_rows for row in rows]
+
+    # Move to processed folder
+    [move_file_to_processed(file) for file in files_to_process]
 
 
-def process_input_files(path):
+def get_input_files(path):
     os.chdir(path)
 
     processed_list = [
-        import_file(file)
-        for (dirpath, dirnames, filenames) in walk(path)
-        for file in filenames
+        file for (dirpath, dirnames, filenames) in walk(path) for file in filenames
     ]
 
-    print(processed_list)
+    return processed_list
 
 
 def import_file(file):
@@ -29,12 +35,17 @@ def import_file(file):
     max_row = sheet_obj.nrows
     initial_row = int(myconstants.INITIAL_ROW)
 
-    # Extrat data for each row and insert into database
-    [get_row_data(sheet_obj.row(cur_row)) for cur_row in range(initial_row, max_row)]
+    # Extract data for each row
+    rows = [sheet_obj.row(cur_row) for cur_row in range(initial_row, max_row)]
 
-    return file
+    return rows
 
 
-def get_row_data(row):
-    # TODO: Extrat fields and insert into database
+def insert_row_data(row):
+    # TODO: Extract fields and insert into database
+    pass
+
+
+def move_file_to_processed(file):
+    # TODO: Move file to processed folder
     pass
